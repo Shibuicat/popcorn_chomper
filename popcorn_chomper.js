0f7a753433b72ef3406bc34ac0a54c7015a1e6a1
Do Tran <vivaladaman@gmail.com>
2024-10-15 21:38:32 +0700

    update to version 1.2, added more domain to block

--- popcorn_chomper.js
+++ popcorn_chomper.js
@@ -1,14 +1,30 @@
-const blockedDomains = ["crab.com"];
+const blockedDomains = {
+};
+
+const blockedDomainsObj = {
+	"xem.vn": true,
+	"mangapark.io": true,
+	"mangaforfree.net": true,
+	"mangaforfree.com": true,
+	"toonily.com": true,
+	"toonily.me": true,
+	"mangapark.io": true,
+	"mangapark.net": true,
+	"mangapark.org": true,
+};
 
 function blockThumbnailImage(requestDetails) {
-	console.log(requestDetails);
-	  return {
-		cancel: true,
-	  };
+	const url = URL.parse(requestDetails.url);
+	const hostname = url.hostname;
+	if (blockedDomainsObj[hostname]) {
+		return {
+			cancel: true,
+		};
+	}
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  blockThumbnailImage,
-  { urls: ["<all_urls>"],},
-  ["blocking"],
+	blockThumbnailImage,
+	{ urls: ["<all_urls>"] },
+	["blocking"],
 );
---
+++ publish.sh
@@ -0,0 +1,1 @@
+zip -r -FS ../popcorn_chomper.zip * --exclude '*.git*' 'publish.sh'
