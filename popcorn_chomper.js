cea33a71c6d33f03bae93b56bb4edd4297a95fc4
Do Tran <vivaladaman@gmail.com>
2024-11-10 21:35:06 +0700

    pump 1.3

--- popcorn_chomper.js
+++ popcorn_chomper.js
@@ -1,30 +1,34 @@
-const blockedDomains = {
-};
+const blockedDomains = {};
 
 const blockedDomainsObj = {
-	"xem.vn": true,
-	"mangapark.io": true,
-	"mangaforfree.net": true,
-	"mangaforfree.com": true,
-	"toonily.com": true,
-	"toonily.me": true,
-	"mangapark.io": true,
-	"mangapark.net": true,
-	"mangapark.org": true,
+  "xem.vn": true,
+  "mangapark.io": true,
+  "mangaforfree.net": true,
+  "mangaforfree.com": true,
+  "toonily.com": true,
+  "toonily.me": true,
+  "toongod.org": true,
+  "mangapark.io": true,
+  "mangapark.net": true,
+  "mangapark.org": true,
+  "toonily.me": true,
+  "manhwa-raw.com": true,
+  "manga18.club": true,
+  "mangabuddy.com": true,
 };
 
 function blockThumbnailImage(requestDetails) {
-	const url = URL.parse(requestDetails.url);
-	const hostname = url.hostname;
-	if (blockedDomainsObj[hostname]) {
-		return {
-			cancel: true,
-		};
-	}
+  const url = URL.parse(requestDetails.url);
+  const hostname = url.hostname;
+  if (blockedDomainsObj[hostname]) {
+    return {
+      cancel: true,
+    };
+  }
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-	blockThumbnailImage,
-	{ urls: ["<all_urls>"] },
-	["blocking"],
+  blockThumbnailImage,
+  { urls: ["<all_urls>"] },
+  ["blocking"],
 );
